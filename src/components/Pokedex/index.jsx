1fa1b861f7dac81e2d3dac99342b22519a097656
LeonardoRorizBessa <leonardo.rorizbessa@gmail.com>
2024-10-09 08:02:56 -0300

    melhorias

--- src/components/Pokedex/index.jsx
+++ src/components/Pokedex/index.jsx
@@ -5,6 +5,14 @@ export default function Pokedex({ filters }) {
   const [pokemons, setPokemons] = useState([]);
   const [filteredPokemons, setFilteredPokemons] = useState([]);
 
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleCardClick = (pokemon) => {
+    setSelectedPokemon(pokemon);
+    setShowModal(true);
+  };
+
   const fetchPokemons = async () => {
     try {
       const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=898');
@@ -93,7 +101,7 @@ export default function Pokedex({ filters }) {
       <div className='pokedex'>
         {filteredPokemons.length > 0 ? (
           filteredPokemons.map(pokemon => (
-            <div key={pokemon.id} className='pokemon-card'>
+            <div key={pokemon.id} className='pokemon-card' onClick={() => handleCardClick(pokemon)}>
               <img src={pokemon.sprites.front_default} alt={pokemon.name} />
               <h3>{pokemon.name}</h3>
               <p>ID: {pokemon.id}</p>
@@ -114,6 +122,21 @@ export default function Pokedex({ filters }) {
           <p>Carregando Pokémons...</p>
         )}
       </div>
+
+      {showModal && selectedPokemon && (
+      <div className="modal">
+        <div className="modal-content">
+          <span className="close" onClick={() => setShowModal(false)}>&times;</span>
+          <h2>{selectedPokemon.name}</h2>
+          <img src={selectedPokemon.sprites.front_default} alt={selectedPokemon.name} />
+          <p>ID: {selectedPokemon.id}</p>
+          <p>Altura: {selectedPokemon.height}</p>
+          <p>Peso: {selectedPokemon.weight}</p>
+          <p>Habilidades: {selectedPokemon.abilities.map(a => a.ability.name).join(', ')}</p>
+          <p>Estatísticas: {selectedPokemon.stats.map(stat => `${stat.stat.name}: ${stat.base_stat}`).join(', ')}</p>
+        </div>
+      </div>
+      )}
     </div>
   );
 }
